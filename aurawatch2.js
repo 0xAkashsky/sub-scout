f1be416c66e4351b037aa231f59d9ae8b971cc43
Akash Gupta <akashtesla@gmail.com>
2026-01-07 00:32:14 +0530

    testing fake keys

--- aurawatch2.js
+++ aurawatch2.js
@@ -6,4 +6,7 @@ const intervalId = setInterval(() => {
     clearInterval(intervalIds);
   }
   i++;
+  STRIPE_SECRET_KEY=sk_test_51HFAKEKEYabcdefghijklmnopqrstuvwxyz
+STRIPE_PUBLISHABLE_KEY=pk_test_51HFAKEKEYabcdefghijklmnopqrstuvwxyz
+
 }, 5000);
